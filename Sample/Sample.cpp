1356ce955fa0d1879a5ee89e3a2b6eb93e76ee80
MIYAWKAI <atoka11nm23@gmail.com>
2021-08-29 14:25:03 +0900

    右辺値に対して左辺値参照の.data()を使用

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -3,25 +3,66 @@
 
 #include <iostream>
 #include <vector>
-#include <tuple>
+
+class Widget {
+public:
+	using DataType = std::vector<double>; 
+		
+	//*thisが左辺値の場合に使用可能
+	DataType& data() &
+	{
+		std::cout << "左辺値参照";
+		return values;
+	}
+
+	//*thisが右辺値の場合に使用可能
+	//DataType data() &&
+	//{
+	//	std::cout << "右辺値参照";
+	//	return std::move(values);
+	//}
+
+private:
+	DataType values;
+};
 
 class Base {
 public:
 	virtual void mf1() const;
 	virtual void mf2(int x);
 	virtual void mf3()&;
-	void mf4() const;
+	virtual void mf4() const;
+	virtual void mf5()&&;
 };
 class Derived : public Base {
 public:
-	virtual void mf1();
-	virtual void mf2(unsigned int x);
-	virtual void mf3()&&;
-	void mf4() const;
+	virtual void mf1()const override;
+	virtual void mf2(int x) override;
+
+	virtual void mf3() & override
+	{
+
+	};
+
+	virtual void mf4() const override;
+
+	virtual void mf5() && override {
+
+	};
 };
 
+Widget makeWidget() {
+
+	Widget* tmp = new Widget;
+
+	return *tmp;
+}
 
 int main(void)
 {
-	
+	Widget w;
+
+	auto vals1 = w.data();
+
+	auto vals2 = makeWidget().data(); //makeWidgetでは右辺値が返ってくるので左辺値参照の.data()は使えない
 }
