93ba112d05b8f3badc8ee25946d8db611b2df526
MIYAWKAI <atoka11nm23@gmail.com>
2021-08-29 14:00:22 +0900

    オーバーライドの条件を満たしていない場合

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -5,46 +5,23 @@
 #include <vector>
 #include <tuple>
 
-
-class Widget
-{
-
+class Base {
 public:
-	Widget() {};
-	Widget(const Widget&) = delete;
-	Widget& operator=(const Widget&) = delete;
+	virtual void mf1() const;
+	virtual void mf2(int x);
+	virtual void mf3()&;
+	void mf4() const;
+};
+class Derived : public Base {
+public:
+	virtual void mf1();
+	virtual void mf2(unsigned int x);
+	virtual void mf3()&&;
+	void mf4() const;
 };
 
-class Widget2
-{
-public:
-	Widget2() {};
-
-private:
-	Widget2(const Widget2&) = delete;
-	Widget2& operator=(const Widget2&) = delete;
-};
-
-
-template<typename T>
-void processPointer(T* ptr) {};
-
-template<>
-void processPointer<double>(double*) = delete;
 
 int main(void)
 {
-	//deleteをpublicに
-	Widget tmp;
-	Widget tmp2 = tmp;//「削除された関数を参照」というワーニングで分かりやすい
-
-	//deleteをprivateに
-	Widget2 tmp3;
-	Widget2 tmp4 = tmp3;//「アクセスできません」というワーニングで分かりにくい
-
-	int a = 10;
-	processPointer<int>(&a);
-
-	double b = 1.0;
-	processPointer<char>(&b);
+	
 }
