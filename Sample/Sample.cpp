bf0478d96a2334e12db3405b78f823e6a9f62117
MIYAWKAI <atoka11nm23@gmail.com>
2021-08-29 16:00:27 +0900

    constexpr

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -3,66 +3,18 @@
 
 #include <iostream>
 #include <vector>
+#include <array>
 
-class Widget {
-public:
-	using DataType = std::vector<double>; 
-		
-	//*thisが左辺値の場合に使用可能
-	DataType& data() &
-	{
-		std::cout << "左辺値参照";
-		return values;
-	}
-
-	//*thisが右辺値の場合に使用可能
-	//DataType data() &&
-	//{
-	//	std::cout << "右辺値参照";
-	//	return std::move(values);
-	//}
-
-private:
-	DataType values;
-};
-
-class Base {
-public:
-	virtual void mf1() const;
-	virtual void mf2(int x);
-	virtual void mf3()&;
-	virtual void mf4() const;
-	virtual void mf5()&&;
-};
-class Derived : public Base {
-public:
-	virtual void mf1()const override;
-	virtual void mf2(int x) override;
-
-	virtual void mf3() & override
-	{
-
-	};
-
-	virtual void mf4() const override;
-
-	virtual void mf5() && override {
-
-	};
-};
-
-Widget makeWidget() {
-
-	Widget* tmp = new Widget;
-
-	return *tmp;
+constexpr int pow(int base, int exp) noexcept
+{
+	return (exp == 0 ? 1 : base * pow(base, exp - 1));//C++11は1行のみ
 }
 
 int main(void)
 {
-	Widget w;
-
-	auto vals1 = w.data();
-
-	auto vals2 = makeWidget().data(); //makeWidgetでは右辺値が返ってくるので左辺値参照の.data()は使えない
+	constexpr int base = 2;
+	base = 4;//constexprを変更してみる
+	constexpr int exp = 3;
+	std::array<int, pow(base, exp)> ary;
+	std::cout << ary.size() << std::endl; // 8
 }
