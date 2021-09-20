c7edc507476d58bca865d86d3610dfbec7a274e1
MIYAWKAI <atoka11nm23@gmail.com>
2021-09-20 17:21:05 +0900

    自動的に生成される特殊メンバ関数を理解する

--- Sample/Sample.cpp
+++ Sample/Sample.cpp
@@ -7,96 +7,56 @@
 #include <thread>
 #include <mutex>
 #include <vector>
-std::mutex m;
 
-void ThreadFuncInt(int thread,int* num) {
-     m.lock();
-    std::cout << "ThreadFuncInt " << thread << std::endl;
-    *num *= 10;
-    std::cout << *num << std::endl;
-    m.unlock();
 
-}
 
-void ThreadFuncVector(int thread, std::vector<int> v) {
-    m.lock();
-    std::cout << "ThreadFuncVector " << thread << std::endl;
-    v[2] *= 10;
-    std::cout << v[2] << std::endl;
-    m.unlock();
-
-}
-
-void func(int thread, std::vector<int> v) {
-    m.lock();
-    std::cout << "ThreadFuncVector " << thread << std::endl;
-    v[2] *= 10;
-    std::cout << v[2] << std::endl;
-    m.unlock();
-
-}
-
-class Smple 
+class Widget1
 {
 public:
-    Smple(bool bFlag) :bCashed(bFlag) {};
+    Widget1(int a):m_a(a){}
 
-    void Calc() const{
-       // std::lock_guard<std::mutex> guard(m);
-        if (!bCashed) 
-        {
-            std::cout << "bCashed False " << std::endl;
-            bCashed = true;
-        }
-        else {
-            std::cout << "bCashed true " << std::endl;
-        }
-
-        return;
-    }
-
-    void Calc2() const 
+    Widget1(Widget1&& original) noexcept
     {
-        std::lock_guard<std::mutex> guard(m);
-        if (!bCashed)
-        {
-            std::cout << "bCashed False " << std::endl;
-            bCashed = true;
-        }
-        else {
-            std::cout << "bCashed true " << std::endl;
-        }
+       /// m_a = original.m_a;
     }
+
+    void print() {
+        std::cout << m_a << std::endl;
+    }
+
 private:
-    mutable bool bCashed;
-    mutable std::mutex m;
+    int m_a;
+
+};
+
+class Widget2
+{
+public:
+    Widget2(int a) :m_a(a) {}
+
+    //テンプレートでムーブコンストラクタを自作した場合
+    template<typename T>
+    Widget2(const T&& original) noexcept
+    {
+        /// m_a = original.m_a;
+    }
+
+    void print() {
+        std::cout << m_a << std::endl;
+    }
+
+private:
+    int m_a;
 };
 
 int main() {
 
-    Smple p(false);
+    Widget1 a(1);
+    Widget1 b(std::move(a));
+    b.print();//正常にmoveされていないので無効値が表示される
 
-
-    int i = 10;
-    std::vector<int> v(3,10);
-
-    std::thread th1(ThreadFuncVector, 1, v);//vectorのoperator[]はスレッドセーフ　std::vector<bool>除く
-    std::thread th2(ThreadFuncInt, 2, &i);//書き込み競合
-
-    std::thread th3(ThreadFuncVector, 3, v);//vectorのoperator[]はスレッドセーフ　std::vector<bool>除く
-    std::thread th4(ThreadFuncInt, 4, &i);//書き込み競合
-
-    std::thread th5([&p]() {p.Calc(); });//書き込み競合の可能性
-    std::thread th6([&p]() {p.Calc(); });//書き込み競合の可能性
-    std::thread th7([&p]() {p.Calc2(); });
-
-    th1.join();
-    th2.join();
-    th3.join();
-    th4.join();
-    th5.join();
-    th6.join();
-    th7.join();
-
+    Widget2 c(1);
+    Widget2 d(std::move(c));//テンプレートメンバ関数が特殊メンバ関数の生成を抑制することはない
+    d.print();//1が表示される
     return 0;
 }
